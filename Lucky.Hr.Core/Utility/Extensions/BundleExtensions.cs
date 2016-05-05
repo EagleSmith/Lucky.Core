ae1bff677436604a73698b1b7f5e1c1d5f78f68a
luckearth <luckearth@d3ef7ed1-3dde-664b-9478-91d4762c111c>
2016-05-05 07:01:58 +0000

    更新 Core 增加 BundleExtensions 扩展

---
+++ Lucky.Hr.Core/Utility/Extensions/BundleExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+
+namespace Lucky.Core.Utility.Extensions
+{
+    public class AsIsBundleOrderer : IBundleOrderer
+    {
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+
+    public static class BundleExtensions
+    {
+        public static Bundle ForceOrdered(this Bundle sb)
+        {
+            sb.Orderer = new AsIsBundleOrderer();
+            return sb;
+        }
+    }
+}
